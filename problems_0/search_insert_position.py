c00d1cb8b233d311654797f1315a9ed4d66ab542
Alisher Windows <alisherjonjamilov@gmail.com>
2023-12-02 21:55:30 +0100

    tests added

--- problems_0/search_insert_position.py
+++ problems_0/search_insert_position.py
@@ -1,5 +1,8 @@
+import unittest
+
+
 class Solution:
-    def searchInsert(self, nums:list, target: int) -> int:
+    def search_insert(self, nums:list, target: int) -> int:
         l, r = 0, len(nums) - 1
         while l <= r:
             m = l + (r - l) // 2
@@ -11,3 +14,14 @@ class Solution:
                 l = m + 1
         return l
 
+class TestSearch(unittest.TestCase):
+
+    def test_search_insert(self):
+        obj = Solution()
+        self.assertEqual(obj.search_insert([1,3,5,7], 3), 1)
+        self.assertEqual(obj.search_insert([1,3,5,7], 2), 1)
+        self.assertEqual(obj.search_insert([1,3,5,7], 0), 0)
+        self.assertEqual(obj.search_insert([1,3,5,7], 8), 4)
+
+if __name__=="__main__":
+    unittest.main()
